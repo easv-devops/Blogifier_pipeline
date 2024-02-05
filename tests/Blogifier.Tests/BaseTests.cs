223a8e4d65302396e86b49d3d298e57dccecd9b4
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-02-05 15:12:24 +0100

    test

--- tests/Blogifier.Tests/BaseTests.cs
+++ tests/Blogifier.Tests/BaseTests.cs
@@ -1,6 +1,16 @@
 namespace Blogifier.Tests;
 
+using System;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+
 public class BaseTests
 {
 
+    [Fact]
+    public void getGreetingMessage() {
+        Environment.SetEnvironmentVariable("DRONE", "true");
+        var result = "Hello, Drone!";
+        Assert.Equal("Hello, Drone!", result);
+    }
 }
